bd8b5b8c5cf84f4dc7ad1fca4900a6a1ab2f3337
Mayan Kumar <mayanprogramming62926@gamil.com>
2022-12-04 21:12:41 +0530

    Added "Calculate Attendance"

--- Main.py
+++ Main.py
@@ -23,8 +23,19 @@ def Get_Maximum_achievable_attendance(current_percentage, next_working_days):
     return Maximum_achievable_attendance
 
 
+def Calculate_Attendance(current_percentage, next_working_days, target_present_days):
+    past_present_days = current_percentage * 100
+    past_working_days = 100
+    total_working_days = past_working_days + next_working_days
+
+    new_percentage = (past_present_days + target_present_days) / \
+        (total_working_days)
+    return new_percentage
+
+
 def Options():
-    opt = ["Get Desired Percentage", "Get Maximum achievable attendance"]
+    opt = ["Get Desired Percentage",
+           "Get Maximum achievable attendance", "Calculate Attendance"]
     for i in range(len(opt)):
         print(f"Enter {i+1} for {opt[i]}")
 
@@ -50,6 +61,19 @@ def Options():
         percentage = "{:.2f}".format(ans*100)
         print(
             f"\nif we go college everyday for the next {next_working_days},\nwe can achieve attendance percentage of : {percentage}")
+
+    elif Choice == 3:
+        print("----Calculate Percentage----")
+        current_percentage = float(input("Current attendance (in decimal) :"))
+        next_working_days = int(input("For next how many work days :"))
+        target_present_days = int(input("How many days we plan to go  :"))
+
+        ans = Calculate_Attendance(
+            current_percentage, next_working_days, target_present_days)
+
+        percentage = "{:.2f}".format(ans*100)
+        print(
+            f"\nif we go college  for the next {target_present_days} in {next_working_days} days,\n we can achieve attendance percentage of : {percentage}")
 
 
 if __name__ == "__main__":
